106e36f1df82cde7def56320c8405c46d8c2a9fd
AdamMigliore <adam.dire@gmail.com>
2020-04-20 20:31:11 -0400

    Added an algorithmic tester and the main which is a wrapper around our tests

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("=========================================================");
+		System.out.println("= Welcome to COMP250 Final Project of Winter 20 tester! =");
+		System.out.println("= Credit go to: @FilouMoteur and @AdamMigliore          =");
+		System.out.println("=========================================================\n");
+		new Algorithmic_test().runTests();
+	}
+
+}
